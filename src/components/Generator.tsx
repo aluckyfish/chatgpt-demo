df4d7f3355d6f09b20ff2c2e5766bb0fa5e5afec
unknown <alanxubackup@gmail.com>
2023-06-08 20:12:30 -0700

    test

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -249,7 +249,7 @@ export default () => {
             class="gen-textarea"
           />
           <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn>
-            Send
+            问
           </button>
           <button title="Clear" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
             <IconClear />
